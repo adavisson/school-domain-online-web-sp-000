d9182d37c1970b234ffd09dfbeea30643096c3a6
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 23:15:31 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,15 @@
 # code here!
 class School
   
-  attr_accessor :roster
+  attr_reader :roster
   
   def initialize(name)
     @name = name
     @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[name] = grade
   end
   
 end
