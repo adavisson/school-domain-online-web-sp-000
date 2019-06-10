839bb5412fc1e6ad1b469a7d11851ba573b21fb9
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 23:19:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,6 +9,9 @@ class School
   end
   
   def add_student(name, grade)
+    if @roster[grade].length > 0
+      @roster[grade] = []
+      @roster[grade] << name
     @roster[grade] = name
   end
   
